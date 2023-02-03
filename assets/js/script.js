f2b79663f53b11ed954943eb31580fccd859cbc5
Dawesie <marie.dawe01967@gmail.com>
2023-02-03 17:49:08 +0000

    add code to playon to show game choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-console.log("connected");
+/** 
+ * Check that the DOM content has been loaded. 
+ * Add event listners to the button 
+ * Pass choice into game area
+ */
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByClassName("butn");
     console.log(buttons);
@@ -18,10 +22,27 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             let yourChoice = this.getAttribute("data-choice");
             console.log(yourChoice);
-
+            playOn(yourChoice)
         })
 
     }
-
-
 })
+
+function playOn(yourChoice) {
+    const choice = ["rock", "paper", "scissors"]
+    console.log(choice);
+    const playerImage = document.getElementById("player-image");
+    playerImage.src = `assets/images/${choice[yourChoice]}.png`;
+    playerImage.alt = choice[yourChoice];
+    console.log(playerImage);
+
+    let computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
+
+    const computerImage = document.getElementById("computer-image");
+    computerImage.src = `assets/images/${choice[computerChoice]}.png`;
+    computerImage.alt = choice[computerChoice];
+    console.log(computerImage);
+
+
+}
