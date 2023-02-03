c2888a34621cf739b156387269d7c1cd37e41e38
Dawesie <marie.dawe01967@gmail.com>
2023-02-03 14:14:56 +0000

    add event listers for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,27 @@
-/**Declare constants varables and DOM elements 
- * and possible choices.
- */
-const buttons = document.getElementsByClassName('butn');
-const buttonChoice = ['rock', 'paper', 'scissors'];
-const playerImage = document.getElementById('player-image');
-const computerImage = document.getElementById('computer-image');
-const playerScore = document.getElementById('score-player');
-const computerScore = document.getElementById('score-computer');
-const messages = document.getElementById('messages')
+console.log("connected");
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByClassName("butn");
+    console.log(buttons);
+    console.log(buttons[0]);
+    console.log(buttons[1]);
+    console.log(buttons[2]);
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-choice") === "0") {
+                alert("you clicked rock")
+
+            } else if (this.getAttribute("data-choice") === "1") {
+                alert("you clicked paper")
+            } else if (this.getAttribute("data-choice") === "2") {
+                alert("you clicked scissors")
+            }
+            let yourChoice = this.getAttribute("data-choice");
+            console.log(yourChoice);
+
+        })
+
+    }
 
 
-
-
-/** Add an event listner for all the buttons 
- */
-for (let button of buttons) {
-    button.addEventListner('click', function () {
-        let playerChoice = this.getAttribute('data-choice');
-    })
-    gameOn(playerChoice);
-}
-
-
-/**the game function, sets the player and computer choices
- * checks the results 
- * updates the score
- */
-function gameOn(playerChoice) {
-    playerImage.src = `assets/image/${choices[playerChoice]}.png`;
-    playerImaage.alt = buttonChoice[playerChoice];
-
-    let computerChoice = Math.floor(Math.random() * 3);
-
-    computerImage.src = `assets/image/${choices[computerChoice]}.png`;
-    computerImage.alt = buttonChoice[computerChoice];
-
-    console.log('computerChoice');
-    console.log('playerChoice')
-
-}
+})
